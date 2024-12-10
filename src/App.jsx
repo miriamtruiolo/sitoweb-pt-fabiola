9f835a3bb0cfb6db311ac9460c48eba5275afb89
miriamtruiolo <miriamtruiolo@gmail.com>
2024-12-10 14:29:24 +0100

    init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import {BrowserRouter, Router, Route} from 'react-router-dom'
+
 import Home from './pages/home/Home'
 import About from './pages/about/About'
 import NotFound from './pages/notFound/NotFound'
@@ -5,11 +7,13 @@ import Contact from './pages/contact/Contact'
 import TestimonialGallery from './pages/testimonialGallery/TestimonialGallery'
 import Plans from './pages/plans/Plans'
 import Nutrition from './pages/nutrition/Nutrition'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+    <Navbar/>
       <Home/>
       <About/>
       <Plans/>
@@ -17,7 +21,7 @@ const App = () => {
       <TestimonialGallery/>
       <Contact/>
       <NotFound/>
-    </div>
+    </BrowserRouter>
   )
 }
 
